c931de16fb316d261477cec077df0da7d9deb4e8
zeduh <jose@setorinformatica.com>
2024-07-16 17:39:12 -0300

    Setting Up & Using Parallel Routes

--- starting-project/app/news/[slug]/page.js
+++ starting-project/app/news/[slug]/page.js
@@ -1,7 +1,7 @@
 import { DUMMY_NEWS } from "@/dummy-news";
 import { notFound } from "next/navigation";
 
-export default function NewsDetailPage({ params }){
+export default function NewsDetailPage({ params }) {
   const newsSlug = params.slug;
   const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsSlug);
 
