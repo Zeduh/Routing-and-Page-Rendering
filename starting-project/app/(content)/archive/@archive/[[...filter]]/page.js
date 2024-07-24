b4c4c49a60f41c9742fda41a84c28ac530663328
zeduh <jose@setorinformatica.com>
2024-07-24 16:08:12 -0300

    Migrating An Entire Application To A Local Data Source (Database)

--- starting-project/app/(content)/archive/@archive/[[...filter]]/page.js
+++ starting-project/app/(content)/archive/@archive/[[...filter]]/page.js
@@ -3,23 +3,23 @@ import Link from "next/link";
 import NewsList from "@/components/news-list";
 import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
 
-export default function FilteredNewsPage({ params }) {
+export default  async function FilteredNewsPage({ params }) {
   const filter = params.filter;
 
   const selectedYear = filter?.[0];
   const selectedMonth = filter?.[1];
 
   let news;
-  let links = getAvailableNewsYears();
+  let links = await getAvailableNewsYears();
 
 
   if(selectedYear && !selectedMonth) {
-    news = getNewsForYear(selectedYear);
+    news =  await getNewsForYear(selectedYear);
     links = getAvailableNewsMonths(selectedYear);
   }
 
   if(selectedYear && selectedMonth) {
-    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
     links = [];
   }
 
@@ -29,9 +29,11 @@ export default function FilteredNewsPage({ params }) {
     newsContent = <NewsList news={news} />
   }
 
+  const avaliableYears = await getAvailableNewsYears();
+
   if(
-    selectedYear && !getAvailableNewsYears().includes(+selectedYear) ||
-    selectedMonth && !getAvailableNewsMonths(selectedYear).includes(+selectedMonth)
+    selectedYear && !avaliableYears.includes(selectedYear) ||
+    selectedMonth && !getAvailableNewsMonths(selectedYear).includes(selectedMonth)
   ) {
     throw new Error("Invalid filter.");
   }
